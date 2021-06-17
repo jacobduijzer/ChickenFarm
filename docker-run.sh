e59383a6e50164581b802d97da5f4b0728cb5cb6
Jacob Duijzer <jacob@duijzer.com>
2021-06-17 22:38:44 +0200

    Removed docker files, will use tye for now

--- docker-run.sh
+++
@@ -1,4 +0,0 @@
-docker-compose  -f docker-compose.tools.yml \
-                -f docker-compose.frontend.yml \
-                -f docker-compose.postgresql.yml \
-                -f docker-compose.taskservice.yml $1
